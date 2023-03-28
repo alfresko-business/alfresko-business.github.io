2d78b71121d977ba9b4837e31220f959078643d7
User <vvnzylt@gmail.com>
2023-03-28 11:50:16 +0800

    Minor changes to chocolate_hills files

---
+++ js/chocolate_hills.js
@@ -0,0 +1,16 @@
+// JavaScript //
+var header_logo = document.getElementById("header_logo");
+var home_button = document.getElementById("home_button");
+var contact_button = document.getElementById("contact_button");
+
+header_logo.onclick = function() {
+    window.location.href = "index.html";
+}
+
+home_button.onclick = function() {
+    window.location.href = "index.html";
+}
+
+contact_button.onclick = function() {
+    window.location.href = "contact.html";
+}
