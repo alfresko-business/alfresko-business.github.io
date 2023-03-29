6ee4a72afe548b672c5b0b22ecf73c295fa2a09a
User <vvnzylt@gmail.com>
2023-03-29 08:05:34 +0800

    Update header and footer on HTML, CSS, and JavaScript for chocolate_hills

--- js/chocolate_hills.js
+++ js/chocolate_hills.js
@@ -1,4 +1,11 @@
 // JavaScript //
+var scroll_to_top_button = document.getElementById("scroll_to_top_button");
+scroll_to_top_button.onclick = function() {
+    document.getElementById('header').scrollIntoView();
+}
+
+
+
 var header_logo = document.getElementById("header_logo");
 var home_button = document.getElementById("home_button");
 var contact_button = document.getElementById("contact_button");
