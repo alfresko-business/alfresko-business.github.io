64a00f6254b26a222a982898581907730032abb7
User <vvnzylt@gmail.com>
2023-03-30 02:49:11 +0800

    Update cards for different tourist spots

--- js/main.js
+++ js/main.js
@@ -30,24 +30,22 @@ contact_button.onclick = function() {
 
 // Code for four color buttons
 // This code will help user to identify what is the currently selected color button based on user's preference
-var green_button = document.getElementById("button_green");
-var yellow_button = document.getElementById("button_yellow");
-var brown_button = document.getElementById("button_brown");
-var red_button = document.getElementById("button_red");
 const tourist_spot_cards_green = document.getElementsByClassName("tourist_spot_cards_green");
 const tourist_spot_cards_yellow = document.getElementsByClassName("tourist_spot_cards_yellow");
 const tourist_spot_cards_brown = document.getElementsByClassName("tourist_spot_cards_brown");
 const tourist_spot_cards_red = document.getElementsByClassName("tourist_spot_cards_red");
+var green_button = document.getElementById("button_green");
+var yellow_button = document.getElementById("button_yellow");
+var brown_button = document.getElementById("button_brown");
+var red_button = document.getElementById("button_red");
+
 
 green_button.onclick = function() {
     for (let i = 0; i < tourist_spot_cards_green.length; i++) {
         tourist_spot_cards_green[i].classList.remove("tourist_spot_cards_green_hide");
-        // tourist_spot_cards_yellow[i].classList.add("tourist_spot_cards_yellow_hide");
-        // tourist_spot_cards_red[i].classList.add("tourist_spot_cards_red_hide");
-    }
-
-    for (let i = 0; i < tourist_spot_cards_brown.length; i++) {
+        tourist_spot_cards_yellow[i].classList.add("tourist_spot_cards_yellow_hide");
         tourist_spot_cards_brown[i].classList.add("tourist_spot_cards_brown_hide");
+        tourist_spot_cards_red[i].classList.add("tourist_spot_cards_red_hide");
     }
 
 
@@ -65,6 +63,9 @@ green_button.onclick = function() {
 yellow_button.onclick = function() {
     for (let i = 0; i < tourist_spot_cards_yellow.length; i++) {
         tourist_spot_cards_yellow[i].classList.remove("tourist_spot_cards_yellow_hide");
+        tourist_spot_cards_green[i].classList.add("tourist_spot_cards_green_hide");
+        tourist_spot_cards_brown[i].classList.add("tourist_spot_cards_brown_hide");
+        tourist_spot_cards_red[i].classList.add("tourist_spot_cards_red_hide");
     }
 
     yellow_button.style.backgroundColor = "#caba73";
@@ -80,14 +81,10 @@ yellow_button.onclick = function() {
 
 brown_button.onclick = function() {
     for (let i = 0; i < tourist_spot_cards_brown.length; i++) {
-        tourist_spot_cards_green[i].classList.add("tourist_spot_cards_green_hide");
         tourist_spot_cards_brown[i].classList.remove("tourist_spot_cards_brown_hide");
-        // tourist_spot_cards_yellow[i].classList.add("tourist_spot_cards_yellow_hide");
-        // tourist_spot_cards_red[i].classList.add("tourist_spot_cards_red_hide");
-    }
-
-    for (let i = 0; i < tourist_spot_cards_green.length; i++) {
         tourist_spot_cards_green[i].classList.add("tourist_spot_cards_green_hide");
+        tourist_spot_cards_yellow[i].classList.add("tourist_spot_cards_yellow_hide");
+        tourist_spot_cards_red[i].classList.add("tourist_spot_cards_red_hide");
     }
 
     brown_button.style.backgroundColor = "#c4997f";
@@ -102,6 +99,13 @@ brown_button.onclick = function() {
 }
 
 red_button.onclick = function() {
+    for (let i = 0; i < tourist_spot_cards_red.length; i++) {
+        tourist_spot_cards_red[i].classList.remove("tourist_spot_cards_red_hide");
+        tourist_spot_cards_green[i].classList.add("tourist_spot_cards_green_hide");
+        tourist_spot_cards_yellow[i].classList.add("tourist_spot_cards_yellow_hide");
+        tourist_spot_cards_brown[i].classList.add("tourist_spot_cards_brown_hide");
+    }
+
     red_button.style.backgroundColor = "#d17474";
     red_button.style.color = "black";
 
