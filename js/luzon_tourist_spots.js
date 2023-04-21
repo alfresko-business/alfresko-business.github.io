0d6e212b1e1abdd672c588d12f08840b1ddf85ce
vvnzylt <122538834+vvnzylt@users.noreply.github.com>
2023-04-21 15:03:51 +0800

    Creation of various pages for tourist spots

---
+++ js/luzon_tourist_spots.js
@@ -0,0 +1,25 @@
+// JavaScript //
+var header_logo = document.getElementById("header_logo");
+var home_button = document.getElementById("home_button");
+var contact_button = document.getElementById("contact_button");
+var footer_logo = document.getElementById("footer_logo");
+
+header_logo.onclick = function() {
+    window.location.href = "index.html";
+}
+
+home_button.onclick = function() {
+    window.location.href = "index.html";
+}
+
+shop_button.onclick = function() {
+    window.location.href = "shop.html";
+}
+
+contact_button.onclick = function() {
+    window.location.href = "mailto:alfresko.business@gmail.com";
+}
+
+footer_logo.onclick = function() {
+    window.location.href = "index.html";
+}
