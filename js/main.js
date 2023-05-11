c72ec81f24828014cd578708b0b0c81443d8254b
vvnzylt <122538834+vvnzylt@users.noreply.github.com>
2023-05-12 00:51:25 +0800

    Major changes to shop content
    Subtotal generation on `shop_modal_purchased_order_window` is not working properly.

--- js/main.js
+++ js/main.js
@@ -62,6 +62,7 @@ mindanao_card_button.onclick = function() {
 
 
 // Checkout window
+var shop_modal_checkout_window = document.getElementById("shop_modal_checkout_window");
 var order_now_button = document.getElementById("order_now_button");
 var shop_modal = document.getElementById("shop_modal");
 var body = document.querySelector("body");
@@ -81,6 +82,10 @@ shop_modal.addEventListener("click", (event) => {
         setTimeout(function() {
             shop_modal.style.display = "none";
             shop_modal.classList.remove("close");
+            shop_modal_checkout_window.classList.remove("close");
+            shop_modal_checkout_window.style.display = "flex";
+            shop_modal_purchased_order_window.style.display = "none";
+            shop_modal_purchased_order_window.classList.remove("close");
             body.style.overflow = "auto";
         }, 100);
     }
@@ -98,13 +103,13 @@ smscw_card_order_cupsize.forEach(button => {
 
 // Item counter
 // Get all the input number and checkbox elements
-const numbers = document.querySelectorAll('.smscw_card_order_chosenquantity');
+const quantity = document.querySelectorAll('.smscw_card_order_chosenquantity');
 const checkboxes = document.querySelectorAll('.smcw_card_checkbox');
 const PRICE_PER_ITEM = 15;
 const TIME_PER_ITEM = 15;
 
 // Add event listeners to each input number and checkbox
-numbers.forEach(number => {
+quantity.forEach(number => {
     number.addEventListener('input', updateTotal);
 });
 
@@ -115,7 +120,7 @@ checkboxes.forEach(checkbox => {
 // Update the total value whenever an input number or checkbox is changed
 function updateTotal() {
     let count = 0;
-    numbers.forEach((number, index) => {
+    quantity.forEach((number, index) => {
         if (checkboxes[index].checked) {
             count += Number(number.value);
         }
@@ -134,3 +139,100 @@ function updateTotal() {
         document.getElementById('smcw_ppc_preparationtime').textContent = `About ${minutes}m ${seconds}s`;
   }
 }
+
+// Order confirmation window
+var smcw_ppc_pay_now_button = document.getElementById("smcw_ppc_pay_now_button");
+smcw_ppc_pay_now_button.onclick = function() {
+    shop_modal_checkout_window.classList.add("close");
+    setTimeout(function() {
+        shop_modal_checkout_window.style.display = "none";
+    }, 500);
+    shop_modal_purchased_order_window.style.display = "flex";
+    setTimeout(function() {
+        shop_modal_purchased_order_window.classList.add("open");
+    }, 500);
+    body.style.overflow = "hidden";
+}
+
+// Printing order details
+const orderCheckboxes = document.querySelectorAll('.smcw_card_checkbox');
+const quantityInputs = document.querySelectorAll('.smscw_card_order_chosenquantity');
+const orderDetails = document.querySelectorAll('.smpow_odc_ow_order_details');
+
+function updateOrderDetails(index) {
+    if (checkboxes[index].checked) {
+        const name = checkboxes[index].getAttribute('drink_name');
+        const quantity = quantityInputs[index].value;
+        orderDetails[index].textContent = `${name} x ${quantity} (12oz)`;
+    } else {
+        orderDetails[index].textContent = '';
+    }
+}
+
+checkboxes.forEach((checkbox, index) => {
+    checkbox.addEventListener('change', () => {
+        updateOrderDetails(index);
+    });
+});
+
+quantityInputs.forEach((input, index) => {
+    input.addEventListener('input', () => {
+        updateOrderDetails(index);
+    });
+});
+
+// Display text based on chosen payment method
+const paymentMethod = document.querySelectorAll('input[type="radio"][name="payment_method"]');
+const chosenPaymentMethod = document.getElementById('smpow_odc_ow_payment_method');
+
+paymentMethod.forEach(radio => {
+    radio.addEventListener('change', () => {
+        const payment_method = radio.value;
+        let text;
+        switch (payment_method) {
+            case 'Physical Cash':
+                text = 'Physical Cash';
+                break;
+            case 'Credit/Debit Card':
+                text = 'Credit/Debit Card';
+                break;
+            case 'GCash':
+                text = 'GCash';
+                break;
+            case 'Maya':
+                text = 'Maya';
+                break;
+            case 'Payment Center':
+                text = 'Payment Center';
+                break;
+            default:
+                text = '';
+                break;
+        }
+        chosenPaymentMethod.textContent = text;
+    });
+});
+
+// Generate subtotal
+function calculateSubtotal() {
+    let count = 0;
+    quantity.forEach((number, index) => {
+        if (checkboxes[index].checked) {
+            count += Number(number.value);
+        }
+    });
+    document.getElementById('smpow_odc_ow_subtotal').textContent = "₱" + count * PRICE_PER_ITEM;
+}
+calculateSubtotal();
+
+// Generate reference code based on date
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+const day = ('0' + currentDate.getDate()).slice(-2);
+const hours = ('0' + currentDate.getHours()).slice(-2);
+const minutes = ('0' + currentDate.getMinutes()).slice(-2);
+const seconds = ('0' + currentDate.getSeconds()).slice(-2);
+const referenceCode = `${year}${month}${day}_${hours}${minutes}${seconds}`;
+
+document.getElementById('smpow_odc_ow_reference_code').textContent = referenceCode;
