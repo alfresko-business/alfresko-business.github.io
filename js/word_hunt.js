faef92dbec85841d674dacf1bd31b650e1a0ebec
vvnzylt <122538834+vvnzylt@users.noreply.github.com>
2023-05-13 17:43:17 +0800

    More changes to word hunt game

--- js/word_hunt.js
+++ js/word_hunt.js
@@ -38,3 +38,31 @@ user_login_button.onclick = function() {
 footer_logo.onclick = function() {
     window.location.href = "index.html";
 }
+
+// Game window
+initializeGameWindow();
+function initializeGameWindow() {
+    var game_modal = document.getElementById("game_modal");
+    var play_button = document.getElementById("play_button");
+    var body = document.querySelector("body");
+
+    play_button.onclick = function() {
+        game_modal.style.display = "block";
+        setTimeout(function() {
+            game_modal.classList.add("open");
+        }, 50);
+        body.style.overflow = "hidden";
+    }
+    
+    game_modal.addEventListener("click", (event) => {
+        if (event.target === game_modal) {
+            game_modal.classList.remove("open");
+            game_modal.classList.add("close");
+            setTimeout(function() {
+                game_modal.style.display = "none";
+                game_modal.classList.remove("close");
+                body.style.overflow = "auto";
+            }, 100);
+        }
+    });
+}
