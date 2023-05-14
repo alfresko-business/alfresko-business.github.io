937ca6bd3e1e9d2e5b2710cec253e7bee14d5a0f
vvnzylt <122538834+vvnzylt@users.noreply.github.com>
2023-05-14 12:44:00 +0800

    Replace `main` to `index` of JS and CSS files

--- js/index.js
+++ js/index.js
@@ -8,43 +8,6 @@ scroll_to_tourist_destination_section_button.onclick = function() {
     document.getElementById('tourist_destination_section').scrollIntoView();
 }
 
-var scroll_to_top_button = document.getElementById("scroll_to_top_button");
-scroll_to_top_button.onclick = function() {
-    document.getElementById('welcome_section').scrollIntoView();
-}
-
-var header_logo = document.getElementById("header_logo");
-var home_button = document.getElementById("home_button");
-var shop_button = document.getElementById("shop_button");
-var contact_button = document.getElementById("contact_button");
-var user_login_button = document.getElementById("user_login_button");
-var footer_logo = document.getElementById("footer_logo");
-
-header_logo.onclick = function() {
-    window.location.href = "index.html";
-}
-
-home_button.onclick = function() {
-    window.location.href = "index.html";
-}
-
-shop_button.onclick = function() {
-    window.location.href = "index.html#shop_section";
-}
-
-contact_button.onclick = function() {
-    window.location.href = "mailto:alfresko.business@gmail.com";
-}
-
-user_login_button.onclick = function() {
-    window.location.href = "authentication.html";
-}
-
-footer_logo.onclick = function() {
-    window.location.href = "index.html";
-}
-
-
 var luzon_card_button = document.getElementById("luzon_card_button");
 luzon_card_button.onclick = function() {
     window.location.href = "luzon_tourist_spots.html"
