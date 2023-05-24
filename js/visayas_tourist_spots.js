e62cedbc9ec26f659be390d950d151be2df0ec37
vvnzylt <122538834+vvnzylt@users.noreply.github.com>
2023-05-24 20:42:56 +0800

    Add content for Boracay Island

--- js/visayas_tourist_spots.js
+++ js/visayas_tourist_spots.js
@@ -6,7 +6,12 @@ scroll_to_top_button.onclick = function() {
 
 // For cards to make it clickable and redirect it to their corresponding pages
 var cards_chocolate_hills = document.getElementById("cards_chocolate_hills");
+var cards_boracay_island = document.getElementById("cards_boracay_island");
 
 cards_chocolate_hills.onclick = function() {
     window.location.href = "chocolate_hills.html";
+}
+
+cards_boracay_island.onclick = function() {
+    window.location.href = "boracay_island.html";
 }
