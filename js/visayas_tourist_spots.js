378e1d4edd61a7baf78781c3b79fc2473c349404
vvnzylt <122538834+vvnzylt@users.noreply.github.com>
2023-05-24 21:12:23 +0800

    Add content for The Ruins, Negros Occidental

--- js/visayas_tourist_spots.js
+++ js/visayas_tourist_spots.js
@@ -7,6 +7,7 @@ scroll_to_top_button.onclick = function() {
 // For cards to make it clickable and redirect it to their corresponding pages
 var cards_chocolate_hills = document.getElementById("cards_chocolate_hills");
 var cards_boracay_island = document.getElementById("cards_boracay_island");
+var cards_the_ruins = document.getElementById("cards_the_ruins");
 
 cards_chocolate_hills.onclick = function() {
     window.location.href = "chocolate_hills.html";
@@ -14,4 +15,8 @@ cards_chocolate_hills.onclick = function() {
 
 cards_boracay_island.onclick = function() {
     window.location.href = "boracay_island.html";
+}
+
+cards_the_ruins.onclick = function() {
+    window.location.href = "the_ruins.html";
 }
