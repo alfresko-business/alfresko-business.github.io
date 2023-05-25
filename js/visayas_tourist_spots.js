aa9f6cdcaebd7e1a4639009fd7d8d969957697d7
vvnzylt <122538834+vvnzylt@users.noreply.github.com>
2023-05-25 22:48:06 +0800

    Add content for Red Rock Hot Spring

--- js/visayas_tourist_spots.js
+++ js/visayas_tourist_spots.js
@@ -9,6 +9,7 @@ var cards_chocolate_hills = document.getElementById("cards_chocolate_hills");
 var cards_boracay_island = document.getElementById("cards_boracay_island");
 var cards_the_ruins = document.getElementById("cards_the_ruins");
 var cards_kawasan_falls = document.getElementById("cards_kawasan_falls");
+var cards_red_rock_hot_spring = document.getElementById("cards_red_rock_hot_spring");
 
 cards_chocolate_hills.onclick = function() {
     window.location.href = "chocolate_hills.html";
@@ -24,4 +25,8 @@ cards_the_ruins.onclick = function() {
 
 cards_kawasan_falls.onclick = function() {
     window.location.href = "kawasan_falls.html";
+}
+
+cards_red_rock_hot_spring.onclick = function() {
+    window.location.href = "red_rock_hot_spring.html";
 }
