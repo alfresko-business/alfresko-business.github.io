3e48c1e80298b163f61e73babf4c42f0dd836cb0
vvnzylt <122538834+vvnzylt@users.noreply.github.com>
2023-05-25 21:39:15 +0800

    Add content for Lake Agco

--- js/mindanao_tourist_spots.js
+++ js/mindanao_tourist_spots.js
@@ -7,7 +7,7 @@ scroll_to_top_button.onclick = function() {
 var cards_aliwagwag_falls = document.getElementById("cards_aliwagwag_falls");
 var cards_lake_sebu = document.getElementById("cards_lake_sebu");
 var cards_mount_apo = document.getElementById("cards_mount_apo");
-
+var cards_lake_agco = document.getElementById("cards_lake_agco");
 
 cards_aliwagwag_falls.onclick = function() {
     window.location.href = "aliwagwag_falls.html";
@@ -19,4 +19,8 @@ cards_lake_sebu.onclick = function() {
 
 cards_mount_apo.onclick = function() {
     window.location.href = "mount_apo.html";
+}
+
+cards_lake_agco.onclick = function() {
+    window.location.href = "lake_agco.html";
 }
