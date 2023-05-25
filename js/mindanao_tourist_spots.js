7863bf0e1c6c181f2a65504299b7c93f03b3fbed
vvnzylt <122538834+vvnzylt@users.noreply.github.com>
2023-05-25 20:18:33 +0800

    Add content for Lake Sebu

--- js/mindanao_tourist_spots.js
+++ js/mindanao_tourist_spots.js
@@ -5,8 +5,13 @@ scroll_to_top_button.onclick = function() {
 
 // For cards to make it clickable and redirect it to their corresponding pages
 var cards_aliwagwag_falls = document.getElementById("cards_aliwagwag_falls");
+var cards_lake_sebu = document.getElementById("cards_lake_sebu");
 
 
 cards_aliwagwag_falls.onclick = function() {
     window.location.href = "aliwagwag_falls.html";
+}
+
+cards_lake_sebu.onclick = function() {
+    window.location.href = "lake_sebu.html";
 }
