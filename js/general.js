43a02f1aad962f70f82f1752860522aba3a5bc83
vvnzylt <122538834+vvnzylt@users.noreply.github.com>
2023-06-01 20:42:35 +0800

    Implement responsive web design for homepage

--- js/general.js
+++ js/general.js
@@ -6,10 +6,14 @@ document.documentElement.style.setProperty('--header-height', `${headerHeight}px
 var header_logo = document.getElementById("header_logo");
 var home_button = document.getElementById("home_button");
 var shop_button = document.getElementById("shop_button");
-var contact_button = document.getElementById("contact_button");
+var sh_home_button = document.getElementById("sh_home_button");
+var sh_shop_button = document.getElementById("sh_shop_button");
+var sh_user_login_button = document.getElementById("sh_user_login_button");
 var user_login_button = document.getElementById("user_login_button");
 var footer_logo = document.getElementById("footer_logo");
 var footer_email = document.getElementById("footer_email");
+var hamburger_button = document.getElementById("hamburger_button");
+var sub_header = document.getElementById("sub_header");
 
 header_logo.onclick = function() {
     window.location.href = "index.html";
@@ -19,12 +23,34 @@ home_button.onclick = function() {
     window.location.href = "index.html";
 }
 
+sh_home_button.onclick = function() {
+    window.location.href = "index.html";
+}
+
 shop_button.onclick = function() {
+    window.location.href = "index.html#shop_section";
+}
+
+sh_shop_button.onclick = function() {
     window.location.href = "index.html#shop_section";
 }
 
 user_login_button.onclick = function() {
     window.location.href = "authentication.html";
+}
+
+sh_user_login_button.onclick = function() {
+    window.location.href = "authentication.html";
+}
+
+hamburger_button.onclick = function() {
+    if (sub_header.style.display === "flex") {
+        sub_header.style.display = "none";
+    } else {
+        sub_header.style.display = "flex";
+    }
+
+    document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
 }
 
 footer_logo.onclick = function() {
@@ -34,3 +60,11 @@ footer_logo.onclick = function() {
 footer_email.onclick = function() {
     window.location.href = "mailto:alfresko.business@gmail.com";
 }
+
+window.addEventListener('resize', function () {
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    if (screenWidth > 576) {
+        sub_header.style.display = 'none';
+    }
+});
